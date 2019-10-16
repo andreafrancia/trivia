3432d4c4779b68e04734a5b12f7f8fe387ab2bbb
alderon7 <56629595+alderon7@users.noreply.github.com>
2019-10-16 12:41:15 +0200

    refactor

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -124,15 +124,11 @@ namespace UglyTrivia
 
         string CurrentCategory()
         {
-            if (places[currentPlayer] == 0) return "Pop";
-            if (places[currentPlayer] == 4) return "Pop";
-            if (places[currentPlayer] == 8) return "Pop";
-            if (places[currentPlayer] == 1) return "Science";
-            if (places[currentPlayer] == 5) return "Science";
-            if (places[currentPlayer] == 9) return "Science";
-            if (places[currentPlayer] == 2) return "Sports";
-            if (places[currentPlayer] == 6) return "Sports";
-            if (places[currentPlayer] == 10) return "Sports";
+            var place = places[currentPlayer] %4;
+
+            if (place == 0) return "Pop";
+            if (place == 1) return "Science";
+            if (place == 2) return "Sports";
             return "Rock";
         }
 
